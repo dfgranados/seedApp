3c35253ce1ec53da63c8c73c6cbd35b274886d20
Dan Granados <dfgranadosreyes@gmail.com>
2014-09-30 17:12:57 -0400

    seedApp for phonegap localyogi, side menu and bottom tabs work

---
+++ www/js/controllers.js
@@ -0,0 +1,12 @@
+angular.module('seedApp.controllers', [])
+
+.controller('NavCtrl', function($scope, $ionicSideMenuDelegate) {
+  $scope.showMenu = function () {
+    $ionicSideMenuDelegate.toggleLeft();
+  };
+  $scope.showRightMenu = function () {
+    $ionicSideMenuDelegate.toggleRight();
+  };
+})
+.controller('HomeTabCtrl', function($scope) {
+});
